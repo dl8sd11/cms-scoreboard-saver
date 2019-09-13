366d3ec1506628c21b77e390a5b2dee7afd28a07
dl8sd11 <tmd910607@gmail.com>
2019-09-13 14:08:26 +0800

    parse from args

--- main.py
+++ main.py
@@ -1,34 +1,49 @@
 import requests
 import json
+import os
+import argparse
 
-baseUrl = "https://cms.nehs.hc.edu.tw/rank/34/"
 
 taskList = [
     {
-        "url" : "users",
-        "filename" : "users.json"
+        "url" : "/users",
+        "filename" : "/users.json"
     },
     {
-        "url" : "tasks",
-        "filename" : "tasks.json"
+        "url" : "/tasks",
+        "filename" : "/tasks.json"
     },
     {
-        "url" : "history",
-        "filename" : "history.json"
+        "url" : "/history",
+        "filename" : "/history.json"
     }
 ]
 
 def saveFileToJSON (data, fileName):
-    with open(fileName, 'w', encoding='utf8') as f:
+    with open(contestName + fileName, 'w', encoding='utf8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
 def getData (task):
-    response = requests.get(url = baseUrl + task["url"])
-    print(response.headers)
-    data = response.json()
-    print(task["filename"])
+    data = requests.get(url = baseUrl + task["url"]).json()
     saveFileToJSON(data, task["filename"])
+    print("Got data from " + task["url"])
     return data
+
+parser = argparse.ArgumentParser()
+parser.add_argument('contest_name',type=str)
+parser.add_argument('base_url',type=str)
+args = parser.parse_args()
+
+baseUrl = args.base_url
+contestName = args.contest_name
+
+try:
+    # Create target Directory
+    os.mkdir(contestName)
+    print("Directory " , contestName ,  " created ") 
+except FileExistsError:
+    print("Directory " , contestName ,  " already exists")
+
 
 for task in taskList:
     getData(task)
